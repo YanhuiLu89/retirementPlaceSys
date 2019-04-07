129230d06ee451fcc5bc7574b9ce0be836b47f1d
luyanhui <luyanhui89@126.com>
2019-04-07 18:55:40 +0800

    实现主页到注册和登陆页面的跳转

--- pages/urls.py
+++ pages/urls.py
@@ -5,8 +5,6 @@ app_name='[pages]'
 
 urlpatterns = [
     path('index/', views.index, name='index'),
-    path('index/', views.toregist, name='toregist'),
-    path('index/', views.tologin, name='tologin'),
-    path('login/', views.regist, name='regist'),
-    path('regist/', views.login, name='login'),
+    path('next/', views.index, name='index'),
+    path('regist/', views.index, name='index'),
 ]
--- pages/views.py
+++ pages/views.py
@@ -5,6 +5,15 @@ from django.urls import reverse
 
 # Create your views here.
 def index(request):#index,首页
+    print ('000000000')
+    if request.method == 'POST':     
+        if 'regist' in request.POST:
+            print ('111111111111111111111111111')
+            return render(request, 'pages/regist.html')#跳转到注册页面    
+        elif  'login' in request.POST: 
+            print ('222222222222222222222')
+            return render(request, 'pages/login.html')#跳转到登陆页面     
+    print ('3333333333333333333333333')    
     return render(request, 'pages/index.html')
 
 def toregist(request):#去注册页面
@@ -14,7 +23,7 @@ def tologin(request):#去登陆页面
     return render(request, 'pages/index.html')
 
 def regist(request):#注册
-    return render(request, 'pages/index.html')
+    return render(request, 'pages/regist.html')
 
 def login(request):#登陆
-    return render(request, 'pages/index.html')
+    return render(request, 'pages/login.html')
