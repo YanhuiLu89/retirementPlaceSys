d96efadf0fefa33a1b3300ed45e542239da43b20
luyanhui <luyanhui89@126.com>
2019-04-07 18:07:47 +0800

    布局index界面

--- pages/urls.py
+++ pages/urls.py
@@ -5,4 +5,8 @@ app_name='[pages]'
 
 urlpatterns = [
     path('index/', views.index, name='index'),
+    path('index/', views.toregist, name='toregist'),
+    path('index/', views.tologin, name='tologin'),
+    path('login/', views.regist, name='regist'),
+    path('regist/', views.login, name='login'),
 ]
--- pages/views.py
+++ pages/views.py
@@ -4,5 +4,17 @@ from django.http import HttpResponseRedirect
 from django.urls import reverse
 
 # Create your views here.
-def index(request):
+def index(request):#index,首页
+    return render(request, 'pages/index.html')
+
+def toregist(request):#去注册页面
+    return render(request, 'pages/index.html')
+
+def tologin(request):#去登陆页面
+    return render(request, 'pages/index.html')
+
+def regist(request):#注册
+    return render(request, 'pages/index.html')
+
+def login(request):#登陆
     return render(request, 'pages/index.html')
